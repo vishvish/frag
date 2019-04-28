e254689f226b79a9965087cf6998e85fb38b6bf7
Vish Vishvanath <vishvish@users.noreply.github.com>
2019-04-28 22:34:01 +0100

    Add 10ms delay to allow go scheduler time.

--- cmd/get.go
+++ cmd/get.go
@@ -23,20 +23,17 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
 // getCmd represents the get command
 var getCmd = &cobra.Command{
 	Use:   "get",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Sample command",
+	Long:  `Sample Command.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		s, retrieveError := retrieveItem()
+		time.Sleep(10 * time.Millisecond)
 		if retrieveError != nil {
 			log.Println("Please login first.")
 		} else {
